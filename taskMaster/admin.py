968f5d31472d4d6cf287efd9776d7e527f54ea85
Evers D MBINI <eversdarrellmbini28@gmail.com>
2025-05-20 15:15:52 +0100

    update

--- taskMaster/admin.py
+++ taskMaster/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Tasks, Masters
 
 # Register your models here.
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'description', 'priorite', 'date_creation', 'date_echeance', 'status')
+    search_fields = ('titre', 'description')
+    list_filter = ('status', 'priorite')
+
+class MastersAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenom', 'sexe', 'email')
+    search_fields = ('nom', 'prenom')
+    list_filter = ('sexe',)
+    
+admin.site.register(Tasks, TasksAdmin)   
+admin.site.register(Masters, MastersAdmin)
